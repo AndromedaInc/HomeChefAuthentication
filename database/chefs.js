b7dbd5db1fb1c7c1a13ccbb06e3add2e8ce9ac59
Stephen Zerfas <stephen.zerfas@gmail.com>
2018-10-16 11:54:43 -0700

    Remove old code and comments in teh DB file and configure package.json to specify nodemon inspector port to enable running both HomeChefAuth and HomeChef on local machines at same time

--- database/chefs.js
+++ database/chefs.js
@@ -4,10 +4,7 @@ const checkExistingEmailUsername = (username, email) => {
   const { or } = db.connection.Op;
   return db.Chef.findOne({
     where: {
-      [or]: [
-        { username },
-        { email },
-      ],
+      [or]: [{ username }, { email }],
     },
   });
 };
@@ -21,37 +18,6 @@ const createChef = (username, password, email, name) => db.Chef.create({
   name,
 });
 
-// const getChef = id => db.Chef.findOne({ where: { id } });
-
-// const upsertAccountInfo = ({
-//   city,
-//   description,
-//   id,
-//   imageUrl,
-//   name,
-//   password,
-//   stateName,
-//   streetAddress,
-//   username,
-//   zip,
-// }) => db.Chef.upsert({
-//   city,
-//   description,
-//   id,
-//   imageUrl,
-//   name,
-//   password,
-//   stateName,
-//   streetAddress,
-//   username,
-//   zip,
-// });
-
-// const findChef = username => db.Chef.findOne({ where: { username } });
-
 exports.checkExistingEmailUsername = checkExistingEmailUsername;
 exports.checkUsername = checkUsername;
 exports.createChef = createChef;
-// exports.getChef = getChef;
-// exports.upsertAccountInfo = upsertAccountInfo;
-// exports.findChef = findChef;
